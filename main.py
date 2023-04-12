76640d85e696194aacd703c3696a3b8a5e04ce3c
Mani Kandan <40218068+mkandan@users.noreply.github.com>
2023-04-12 15:13:52 -0700

    feat: downloading yt file!

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from yt_dlp import YoutubeDL
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World","var":123}
 
 @app.get("/test")
-async def root():
-    return {"message": "test World","var":789}
+async def test():
+    URLS = ['https://www.youtube.com/watch?v=gGZmi3UVSOI']
+
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+        }]
+    }
+
+    # with YoutubeDL(ydl_opts) as ydl:
+    #     error_code = ydl.do
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download(URLS)
+
+    return {"done": "done"}
+
+# /list-books
+# /book-by-index/{index} (Ex: /book-by-index/1)
