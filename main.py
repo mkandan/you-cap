7e2ac324a84c08f334ae4d04c9825b6efa1c302b
Mani Kandan <40218068+mkandan@users.noreply.github.com>
2023-04-12 14:51:13 -0700

    2nd route

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World","var":123}
+
+@app.get("/test")
+async def root():
+    return {"message": "test World","var":789}
