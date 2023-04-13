13f3024dab0613f3d1f423833c4fa8fa17e3765f
Mani Kandan <40218068+mkandan@users.noreply.github.com>
2023-04-13 14:25:26 -0700

    working: downloadAndUploadAndGenerateCaption

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from storage3 import create_client as create_client_storage
 from supabase import StorageException, create_client, Client
 import datetime
+import whisper_timestamped as whisper
 
 app = FastAPI()
 
@@ -12,8 +13,19 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/downloadAndUpload")
-async def downloadAndUpload():
+@app.get("/downloadFromStorageAndGenerateNewCaption")
+async def downloadFromStorageAndGenerateNewCaption():
+    # parameters: video_id, desired_language
+    # get default_language by doing a db query on video_id
+    return {"message": "WIP"}
+
+@app.get("/downloadAndUploadAndGenerateCaption")
+async def downloadAndUploadAndGenerateCaption():
+    # parameters: desired_language, download_audio_job_id, generate_caption_job_id
+    desired_language = 'en'
+    download_audio_job_id=36
+    generate_captions_job_id=37
+
     dotenv_path = 'dot.env'
     load_dotenv(dotenv_path)
 
@@ -23,10 +35,6 @@ async def downloadAndUpload():
 
     # pass in is_async=True to create an async client
     storage_client = create_client_storage(url, headers, is_async=False)
-
-    # allFiles = storage_client.from_('audio').list()
-    # print('allFiles', allFiles)
-    # return {"allFiles": allFiles}
 
     URLS = ['https://www.youtube.com/watch?v=gGZmi3UVSOI'] # apple audio
 
@@ -53,14 +61,43 @@ async def downloadAndUpload():
         res = storage_client.from_('audio').upload(f'{file_path}', file_path,)
         print('res', res)
 
-    # prep supabase client for updating history
     supabase: Client = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_API_KEY'))
     # prep updated history with current utc time
     utc_now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    existing_history = supabase.table('audio').select('history').eq('video_id', file_name).execute()
-    updated_history = existing_history.data[0]['history']
-    updated_history.append({'type':'downloaded_audio','time': utc_now})
-
+    audio_existing_history = supabase.table('audio').select('id,history').eq('video_id', file_name).execute()
+    audio_updated_history = audio_existing_history.data[0]['history'] + [{'event':'downloaded_audio','time': utc_now}]
     # update row's history in audio table
-    res = supabase.table('audio').update({"history": updated_history}).eq('video_id', file_name).execute()
-    return {"res": res}
+    res = supabase.table('audio').update({"history": audio_updated_history}).eq('video_id', file_name).execute()
+
+    # update download_audio job's history and status
+    download_audio_job_existing_history = supabase.table('queue').select('history').eq('id', download_audio_job_id).execute()
+    download_audio_job_updated_history = download_audio_job_existing_history.data[0]['history'] + [{'event':'downloaded_audio','time': utc_now}]
+    download_audio_job__res = supabase.table('queue').update({"history": download_audio_job_updated_history, "status": "complete"}).eq('id', download_audio_job_id).execute()
+
+    # run audio through openai whisper
+    whisper_audio = whisper.load_audio(file_path)
+    whisper_model = whisper.load_model('tiny',device='cpu')
+    whisper_res = whisper.transcribe(whisper_model, whisper_audio)
+    captions = []
+    # pull text, start time, end time, confidence (accuracy) for each segment
+    for segment in whisper_res["segments"]:
+        for word in segment["words"]:
+            captions.append({
+                "text": word["text"],
+                "start": word["start"],
+                "end": word["end"],
+                "confidence": word["confidence"]
+            })
+    # write captions to db
+    utc_now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    captions_res = supabase.table('captions').insert({"history":[{'event':"created_at",'time':utc_now},{'event':"generated_captions",'time':utc_now}],"audio_id":audio_existing_history.data[0]['id'],"language":whisper_res["language"],"timestamped_captions":captions}).execute()
+    # update generate_captions job's history and status
+    generate_captions_job_existing_history = supabase.table('queue').select('history').eq('id', generate_captions_job_id).execute()
+    generate_captions_job_updated_history = generate_captions_job_existing_history.data[0]['history'] + [{'event':'generated_captions','time': utc_now}]
+    generate_captions_job__res = supabase.table('queue').update({"history": generate_captions_job_updated_history, "status": "complete"}).eq('id', generate_captions_job_id).execute()
+
+
+    return {"language":whisper_res["language"],"captions_res": captions_res,"whisper_res": whisper_res}
+    
+    # delete file from local storage
+    os.remove(file_path)
