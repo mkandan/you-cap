ca56481987f4c880f68a308bde280b198eae4279
Mani Kandan <40218068+mkandan@users.noreply.github.com>
2023-04-12 22:16:14 -0700

    + dot.env feat: supabase connected

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 from fastapi import FastAPI 
 from yt_dlp import YoutubeDL
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World","var":123}
+    return {"message": "Hello World"}
 
 @app.get("/test")
 async def test():
-    URLS = ['https://www.youtube.com/watch?v=gGZmi3UVSOI']
+    dotenv_path = 'dot.env'
+    load_dotenv(dotenv_path)
 
-    ydl_opts = {
-        'format': 'm4a/bestaudio/best',
-        'postprocessors': [{  # Extract audio using ffmpeg
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'm4a',
-        }]
-    }
+    supabase: Client = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_API_KEY'))
+
+    response = supabase.table('queue').select('*').execute()
+    print(response)
+    return {'response': response}
+
+    # URLS = ['https://www.youtube.com/watch?v=gGZmi3UVSOI']
+
+    # ydl_opts = {
+    #     'format': 'm4a/bestaudio/best',
+    #     'postprocessors': [{  # Extract audio using ffmpeg
+    #         'key': 'FFmpegExtractAudio',
+    #         'preferredcodec': 'm4a',
+    #     }]
+    # }
 
     # with YoutubeDL(ydl_opts) as ydl:
-    #     error_code = ydl.do
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download(URLS)
+    #     ydl.download(URLS)
 
     return {"done": "done"}
 
